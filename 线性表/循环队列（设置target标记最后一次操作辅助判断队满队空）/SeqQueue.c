95c6e0cfefd31c276e9a424778b29ef0def4ae31
Wz-OT <74951280+Wz-OT@users.noreply.github.com>
2022-04-19 22:18:16 +0800

    Create SeqQueue.c

---
+++ 线性表/循环队列（设置target标记最后一次操作辅助判断队满队空）/SeqQueue.c
@@ -0,0 +1,82 @@
+/*
+* 循环队列
+* 设置target标记最后一次操作辅助判断队满队空
+* target == 0 时表示最后一次操作为插入
+* target == 1 时表示最后一次操作为删除
+* 无需损失存储单元
+*/
+#include <stdio.h>
+typedef int bool;
+#define true 1
+#define false 0
+#define MaxSize 10
+//定义一个循环队列
+typedef struct {
+	int data[MaxSize];
+	int front, rear;
+	int target;
+}SeqQueue;
+//初始化
+bool InitQueue(SeqQueue* Q) {
+	Q->rear = 0;
+	Q->front = 0;
+	Q->target = 0;
+	return true;
+}
+//判空
+bool Empty(SeqQueue Q) {
+	if (Q.front == Q.rear && Q.target == 0)
+		return true;
+	else
+		return false;
+}
+//入队
+bool EnQueue(SeqQueue* Q, int e) {
+	if (Q->front == Q->rear && Q->target == 1)
+		return false;
+	Q->data[Q->rear] = e;
+	Q->rear = (Q->rear + 1) % MaxSize;
+	Q->target = 1;
+	return true;
+}
+//出队
+int DeQueue(SeqQueue* Q) {
+	if (Q->front == Q->rear && Q->target == 0)
+		return 9999;
+	int e = Q->data[Q->front];
+	Q->front = (Q->front + 1) % MaxSize;
+	Q->target = 0;
+	return e;
+}
+void main() {
+	SeqQueue Q;
+	InitQueue(&Q);
+	if (Empty(Q))
+		printf("此队列为空！\n");
+	else
+		printf("此队列非空！\n");
+	if (EnQueue(&Q, 1))
+		printf("入队成功！\n");
+	else
+		printf("入队失败！\n");
+	if (EnQueue(&Q, 2))
+		printf("入队成功！\n");
+	else
+		printf("入队失败！\n");
+	if (EnQueue(&Q, 3))
+		printf("入队成功！\n");
+	else
+		printf("入队失败！\n");
+	if (EnQueue(&Q, 4))
+		printf("入队成功！\n");
+	else
+		printf("入队失败！\n");
+	for (int i = Q.front % MaxSize; i < Q.front % MaxSize + 4; i++)
+		printf("%d\n", Q.data[i]);
+	int e = DeQueue(&Q);
+	printf("出队成功！出队元素为%d\n", e);
+	e = DeQueue(&Q);
+	printf("出队成功！出队元素为%d\n", e);
+	for (int i = Q.front % MaxSize; i < Q.front % MaxSize + 2; i++)
+		printf("%d\n", Q.data[i]);
+}
